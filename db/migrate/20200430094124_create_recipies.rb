dd570e73dbe76d7a35c080c7ab7860e8aff5536c
Craig Christopher Messi <craig.messi@gmail.com>
2020-04-30 09:41:44 +0000

    Automatically backed up by Learn

--- db/migrate/20200430094124_create_recipies.rb
+++ db/migrate/20200430094124_create_recipies.rb
@@ -1,4 +1,8 @@
 class CreateRecipies < ActiveRecord::Migration
   def change
+    create_table :recipes do |t|
+      t.string :name
+      t.string :ingredients
+      t.string :cook_time
   end
 end
